08a22c217528ebc427d44856942e856866891ccb
Chris Peka <24318424+cdpuk@users.noreply.github.com>
2022-12-21 22:31:27 +0000

    Fix config flow and auth token storage

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -4,9 +4,23 @@ from unittest.mock import patch
 from homeassistant import config_entries, data_entry_flow
 import pytest
 
-from custom_components.bestway.const import CONF_USERNAME, DOMAIN
+from custom_components.bestway.bestway import BestwayUserToken
+from custom_components.bestway.const import (
+    CONF_API_ROOT,
+    CONF_API_ROOT_EU,
+    CONF_PASSWORD,
+    CONF_USER_TOKEN,
+    CONF_USER_TOKEN_EXPIRY,
+    CONF_USERNAME,
+    DOMAIN,
+)
 
-from .const import MOCK_CONFIG
+# Mock user input to the config flow
+MOCK_USER_INPUT = {
+    CONF_USERNAME: "test@example.org",
+    CONF_PASSWORD: "P@asw0rd",
+    CONF_API_ROOT: CONF_API_ROOT_EU,
+}
 
 
 # This fixture bypasses the actual setup of the integration
@@ -23,7 +37,7 @@ def bypass_setup_fixture():
 
 
 # Simiulate a successful config flow.
-async def test_successful_config_flow(hass, bypass_auth, bypass_get_data):
+async def test_successful_config_flow(hass, bypass_get_data):
     """Test a successful config flow."""
     # Initialize a config flow
     result = await hass.config_entries.flow.async_init(
@@ -34,17 +48,25 @@ async def test_successful_config_flow(hass, bypass_auth, bypass_get_data):
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "user"
 
-    # If a user were to enter a username and password,
-    # it would result in this function call
-    result = await hass.config_entries.flow.async_configure(
-        result["flow_id"], user_input=MOCK_CONFIG
-    )
+    # Mock an authentication call that provides a token to keep hold of
+    token = BestwayUserToken("foo", "t0k3n", 123)
+    with patch(
+        "custom_components.bestway.bestway.BestwayApi.get_user_token",
+        return_value=token,
+    ):
+        result = await hass.config_entries.flow.async_configure(
+            result["flow_id"], user_input=MOCK_USER_INPUT
+        )
+
+    expected_output = dict(MOCK_USER_INPUT)
+    expected_output[CONF_USER_TOKEN] = token.user_token
+    expected_output[CONF_USER_TOKEN_EXPIRY] = token.expiry
 
     # Check that the config flow is complete and a new entry is created with
     # the input data
     assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
-    assert result["title"] == MOCK_CONFIG[CONF_USERNAME]
-    assert result["data"] == MOCK_CONFIG
+    assert result["title"] == MOCK_USER_INPUT[CONF_USERNAME]
+    assert result["data"] == expected_output
     assert result["result"]
 
 
@@ -59,7 +81,7 @@ async def test_failed_config_flow(hass, error_on_auth):
     assert result["step_id"] == "user"
 
     result = await hass.config_entries.flow.async_configure(
-        result["flow_id"], user_input=MOCK_CONFIG
+        result["flow_id"], user_input=MOCK_USER_INPUT
     )
 
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
