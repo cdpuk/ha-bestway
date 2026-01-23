d4a181136c190d1aaef4c3ee89fca23b838af377
cdpuk <24318424+cdpuk@users.noreply.github.com>
2026-01-23 22:16:27 +0000

    Fix tests (#106)

--- tests/test_init.py
+++ tests/test_init.py
@@ -4,15 +4,11 @@ from datetime import datetime, timedelta
 from unittest.mock import patch
 
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
-import pytest
+from homeassistant.config_entries import ConfigEntryState
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.bestway import (
     BestwayUpdateCoordinator,
-    async_reload_entry,
-    async_setup_entry,
-    async_unload_entry,
 )
 from custom_components.bestway.bestway.model import BestwayUserToken
 from custom_components.bestway.const import (
@@ -64,14 +60,15 @@ async def test_setup_unload_and_reload_entry(hass: HomeAssistant, bypass_get_dat
     get_user_token_fn.assert_not_called()
 
     # Reload the entry and assert that the data from above is still there
-    await async_reload_entry(hass, config_entry)
+
+    await hass.config_entries.async_reload(config_entry.entry_id)
     assert DOMAIN in hass.data and config_entry.entry_id in hass.data[DOMAIN]
     assert isinstance(
         hass.data[DOMAIN][config_entry.entry_id], BestwayUpdateCoordinator
     )
 
     # Unload the entry and verify that the data has been removed
-    assert await async_unload_entry(hass, config_entry)
+    assert await hass.config_entries.async_unload(config_entry.entry_id)
     assert config_entry.entry_id not in hass.data[DOMAIN]
 
 
@@ -124,6 +121,9 @@ async def test_setup_entry_exception(hass: HomeAssistant, error_on_get_data):
         version=2,
         entry_id="test",
     )
+    config_entry.add_to_hass(hass)
 
-    with pytest.raises(ConfigEntryNotReady):
-        assert await async_setup_entry(hass, config_entry)
+    await hass.config_entries.async_setup(config_entry.entry_id)
+
+    await hass.async_block_till_done()
+    assert config_entry.state is ConfigEntryState.SETUP_RETRY
